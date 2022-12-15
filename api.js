63af904c11fc19bf3d215a4f33ec8dfd7d40869d
LucasTavrs2 <115112080+LucasTavrs2@users.noreply.github.com>
2022-12-14 23:14:26 -0300

    Add files via upload

---
+++ api.js
@@ -0,0 +1,7 @@
+let api_key = "f1aab682969ee3f6fc3002d8b464329c"
+
+let img_url = "https://image.tmdb.org/t/p/w500"
+
+let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?"
+
+let movie_genres_http = "http://api.themoviedb.org/3/discover/movie?"
